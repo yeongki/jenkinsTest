e6929db83f0cd7a1b5316f0a2fdf434331c170c8
mz-syl <syl@mz.co.kr>
2018-06-29 16:57:45 +0900

    ddd
    ddd

--- src/main/java/com/example/demo/ToDoServiceImpl.java
+++ src/main/java/com/example/demo/ToDoServiceImpl.java
@@ -1,9 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class ToDoServiceImpl implements ToDoService{
